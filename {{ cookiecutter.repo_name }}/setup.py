82fce63014fa1629a1ef465a04776b6ddef615ef
kayibal <alan.f.hoeng@gmail.com>
2017-02-14 12:12:11 +0100

    Fix to install from drtools from datarevenue repo

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -9,7 +9,7 @@ setup(name='{{cookiecutter.pkg_name}}',
       url='{{cookiecutter.project_url}}',
       author='Datarevenue',
       author_email='info@datarevenue.de',
-      dependency_links = ['git+ssh://git@github.com/Haikane/drtools_base.git@master#egg=drtools-{{cookiecutter.drtools_version}}'],
+      dependency_links = ['git+ssh://git@github.com/datarevenue-berlin/drtools_base.git@master#egg=drtools-{{cookiecutter.drtools_version}}'],
       install_requires=['drtools>={{cookiecutter.drtools_version}}'],
       packages=packages,
       zip_safe=False)
